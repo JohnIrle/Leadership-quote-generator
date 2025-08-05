65df91179cca517324865e42ac929580e0e066f5
John Irle <irlejohn@gmail.com>
2025-08-05 11:38:58 -0500

    core: format with prettier

--- src/main.tsx
+++ src/main.tsx
@@ -7,5 +7,5 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>
-)
+  </StrictMode>,
+);
